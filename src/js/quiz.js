033b93fa026341d184256d9670a9373a3959111c
KapilChaudhary <KapilChaudhary706@gmail.com>
2021-11-12 18:50:42 +0530

    BitcoinQuiz

---
+++ src/js/quiz.js
@@ -0,0 +1,109 @@
+let quiz=[
+    {
+        question:"P2P stand for ___________.",
+        option:[
+            "1.Private to Public",
+            "2.Password to Private",
+            "3.Peer to Peer",
+            "4.None of the mentioned",
+        ],
+        answer:3,
+    },
+    {
+        question:"Bitcoin is created by ________",
+        option:[
+            "1.Saifedean Ammous",
+            "2.Satoshi Nakamoto",
+            "3.Vitalik Buterin",
+            "4.None of the mentioned",
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"The term used for a blockchain splits is ________",
+        option:[
+            "1.Merger",
+            "2.fork",
+            "3.Division",
+            "4.All of the mentioned",   
+        ],
+        answer:2,
+    },
+    {
+        question:"_______ is the part of asymmetric encryption",
+        option:[
+            "1.Public Key",
+            "2.Private Key",
+            "3.PassPhrase",
+            "4.All of the aboove",   
+        ],
+        answer:4,
+
+    },
+    {
+        question:"UTXO stands for _______",
+        option:[
+            "1.Unspent Transaction Office",
+            "2.United Transaction Office",
+            "3.United Transaction Output",
+            "4.Unspent Transaction Output",   
+        ],
+        answer:4,
+    },
+    {
+        question:"_________ characteristic makes blockchain tamper-proof",
+        option:[
+            "1.CryptoCurrency",
+            "2.VPN",
+            "3.Immutability",
+            "4.All of the Above",
+            
+        ],
+        answer:3,
+    },
+    {
+        question:"The maximum number of bitcoins that can be created is _________",
+        option:[
+            "1.11 million",
+            "2.25 million",
+            "3.21 million",
+            "4.100 million",
+            
+        ],
+        answer:3,
+    },
+    {
+        question:"Which file separator should be used by MANIFEST file?",
+        option:[
+            "1. /",
+            "2. \\",
+            "3. -",
+            "4. //",
+            
+        ],
+        answer:1,
+    },
+    {
+        question:"Which of the below is not a session tracking method?",
+        option:[
+            "1.URL rewriting",
+            "2.History",
+            "3.Cookies",
+            "4.SSL sessions",
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"Which file is used to define dependency in maven?",
+        option:[
+            "1.build.xml",
+            "2.pom.xml",
+            "3.dependency.xml",
+            "4.version.xml",
+            
+        ],
+        answer:2,
+    }
+]
